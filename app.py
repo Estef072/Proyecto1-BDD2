7fd8e845ad11b1c117af3ae3bc4fcd8dfe5a1950
Priscigs <fatimag811@gmail.com>
2023-02-13 11:15:05 -0600

    app update

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#import database as dbase
+#from recetas import Recipes
+
+#db = dbase.dbConnection()
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
+
+# @app.route('/recetas', methods=['POST'])
+# def addRecipe():
+    # Creando colección si en dado caso no existe
+    #recipes = db['recipes']
+    #id = request.form['id'] 
 
 #Ejecutarse como archivo princpal
 if __name__ == '__main__':
